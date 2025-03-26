fda2fcafb0c777c02392dc2b1e3e2ff01a3426e5
Eric Gao <ericxgao@gmail.com>
2025-03-26 11:43:54 -0700

    fix: reset submodule

--- MockbaModular/helper.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-unset PYTHONHOME
-unset PYTHONPATH
-rm -rf tmp
-mkdir tmp
-../Rack-SDK/helper.py createmodule $1 res/$1.svg tmp/$1.cpp
--- MockbaModular/make.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-RACK_DIR=/home/mockba/VCV/Rack-SDK make $1
-strip plugin.dll
